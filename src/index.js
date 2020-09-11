e582516af7064a2e1d0ef8620a009e04c1d9b1ff
manemejeff <kmb14056@yandex.ru>
2020-09-11 23:42:35 +0400

    app

--- src/index.js
+++ src/index.js
@@ -4,14 +4,33 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Header = () => {
+    return <h2>Hello World!!!</h2>
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Field = () => {
+    return <input type="text" placeholder="Type here"></input>
+}
+
+const Btn = () => {
+    const text = "Log in";
+return <button>{text}</button>
+}
+
+const App_2 = () => {
+    return (
+        <div>
+            <Header/>
+            <Field/>
+            <Btn/>
+        </div>
+        )
+    }
+    
+    ReactDOM.render(<App_2/>, document.getElementById('root'));
+    
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: https://bit.ly/CRA-PWA
+    serviceWorker.unregister();
+    
